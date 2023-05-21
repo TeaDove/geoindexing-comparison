1a8c1f42d779ce1f94fe8d65a919ffc1caa6daa0
pibragimov <pibragimov@whoosh.bike>
2023-05-21 17:27:19 +0300

    auto: autocommit

---
+++ visualisation/draw.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+from fastapi import FastAPI
+from pydantic import Field, BaseModel
+from typing import List
+import uuid
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+
+
+from io import BytesIO
+from numpy import random
+from starlette.responses import StreamingResponse
+import matplotlib
+
+matplotlib.use("agg")
+
+app = FastAPI()
+
+
+class Point(BaseModel):
+    id_: uuid.UUID = Field(default_factory=uuid.uuid4, alias="id")
+    color: str
+
+    lat: float
+    lon: float
+
+
+class Points(BaseModel):
+    __root__: List[Point] = Field(
+        example=[
+            Point(
+                color=random.choice(("Red", "Blue", "Green")),
+                lat=random.rand() * 20 + 40,
+                lon=random.rand() * 20 + 40,
+            )
+            for _ in range(random.randint(10, 20))
+        ]
+    )
+
+
+@app.post("/draw", response_class=StreamingResponse)
+def draw_fig(points: Points) -> StreamingResponse:
+    points_list = [jsonable_encoder(item, by_alias=True) for item in points.__root__]
+    df = pd.DataFrame(points_list)
+    ax = plt.gca()
+    for color in df["color"].unique():
+        colored_df = df[df["color"] == color]
+        colored_df.plot(
+            kind="scatter",
+            x="lon",
+            y="lat",
+            grid=True,
+            stacked=True,
+            ax=ax,
+            color=color,
+            s=10,
+        )
+
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
+
+
+if __name__ == "__main__":
+    uvicorn.run("draw:app")
